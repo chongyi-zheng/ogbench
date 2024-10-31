646083d39633d906893839bea9327349833c3d9f
chongyi-zheng <chongyiz@andrew.cmu.edu>
2024-10-31 15:53:38 -0400

    add env_vars.sh, wandb-osh, and enable_wandb flag

---
+++ env_vars.sh
@@ -0,0 +1,5 @@
+export PYTHONPATH=$HOME/research/ogbench
+export CUDA_VISIBLE_DEVICES=0
+export PATH=$PATH:$CONDA_PREFIX/bin
+export WANDB_API_KEY=bbb3bca410f71c2d7cfe6fe0bbe55a38d1015831
+export MUJOCO_GL=egl
--- impls/utils/log_utils.py
+++ impls/utils/log_utils.py
@@ -6,6 +6,8 @@ import absl.flags as flags
 import ml_collections
 import numpy as np
 import wandb
+import wandb_osh
+from wandb_osh.hooks import TriggerWandbSyncHook
 from PIL import Image, ImageEnhance
 
 
@@ -88,7 +90,12 @@ def setup_wandb(
 
     run = wandb.init(**init_kwargs)
 
-    return run
+    trigger_sync = None
+    if mode == 'offline':
+        wandb_osh.set_log_level("ERROR")
+        trigger_sync = TriggerWandbSyncHook()
+
+    return run, trigger_sync
 
 
 def reshape_video(v, n_cols=None):
