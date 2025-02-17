1c47ca77139258ead304e305766ab40dc0b986f1
chongyi-zheng <chongyiz@andrew.cmu.edu>
2025-02-16 22:05:07 -0500

    update submitit/submit_to_slurm_gcfmrl.py

--- submitit/submit_to_slurm_gcfmrl.py
+++ submitit/submit_to_slurm_gcfmrl.py
@@ -59,7 +59,7 @@ def main():
                                             for distill_type in ['none', 'log_prob', 'noise_div_int']:  # no distillation seems to work better
                                                 for normalize_q_loss in [True]:  # it is important to normalize Q
                                                     for seed in [20]:
-                                                        exp_name = f"{datetime.today().strftime('%Y%m%d')}_gcfmrl_env_name={env_name}_obs_norm_type={obs_norm_type}_alpha={alpha}_const_std={const_std}_solver_type={ode_solver_type}_adjoint_type={ode_adjoint_type}_num_flow_steps={num_flow_steps}_noise_type={noise_type}_div_type={div_type}_distill_type={distill_type}_norm_q_loss={normalize_q_loss}"
+                                                        exp_name = f"{datetime.today().strftime('%Y%m%d')}_gcfmrl_env_name={env_name}_obs_norm={obs_norm_type}_alpha={alpha}_const_std={const_std}_solver={ode_solver_type}_adjoint={ode_adjoint_type}_num_flow_steps={num_flow_steps}_noise={noise_type}_div={div_type}_distill={distill_type}_norm_q={normalize_q_loss}"
                                                         log_dir = os.path.expanduser(
                                                             f"{log_root_dir}/exp_logs/ogbench_logs/gcfmrl/{exp_name}/{seed}")
 
