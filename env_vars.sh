220d99b44d55f60dbf4638e03ae452ac2d2cdb81
chongyi-zheng <chongyiz@andrew.cmu.edu>
2025-02-17 20:07:15 -0500

    update env vars

--- env_vars.sh
+++ env_vars.sh
@@ -5,3 +5,5 @@ export WANDB_API_KEY=bbb3bca410f71c2d7cfe6fe0bbe55a38d1015831
 export MUJOCO_GL=egl
 export PYOPENGL_PLATFORM=egl
 export EGL_DEVICE_ID=0
+export D4RL_SUPPRESS_IMPORT_ERROR=1
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.mujoco/mujoco210/bin:/usr/lib/nvidia
