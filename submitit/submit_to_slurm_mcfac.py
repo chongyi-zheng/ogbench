dc93690a1d08595c952b91499294e9a01651a31f
chongyi-zheng <chongyiz@andrew.cmu.edu>
2025-03-10 14:07:11 -0400

    update submitit/submit_to_slurm_mcfac.py

--- submitit/submit_to_slurm_mcfac.py
+++ submitit/submit_to_slurm_mcfac.py
@@ -46,7 +46,7 @@ def main():
     with executor.batch():  # job array
         for env_name in [
             "antmaze-large-navigate-singletask-v0",
-            "humanoidmaze-medium-navigate-singletask-v0",
+            # "humanoidmaze-medium-navigate-singletask-v0",
             # "antsoccer-arena-navigate-singletask-v0"
             # "antmaze-medium-play-v2",
             # "pen-human-v1",
@@ -58,93 +58,95 @@ def main():
         ]:
             for obs_norm_type in ['none']:
                 for discount in [0.99]:
-                    for alpha in [30, 10, 3]:
-                        for distill_type in ['fwd_sample']:
-                            for distill_mixup in [False]:
-                                for critic_loss_type in ['expectile']:
-                                    for critic_noise_type in ['normal']:
-                                        for expectile in [0.9, 0.95, 0.99]:
-                                            for q_agg in ['min']:
-                                                for normalize_q_loss in [True, False]:
-                                                    for reward_layer_norm in [True, False]:
-                                                        for use_target_reward in [True, False]:
-                                                            for reward_type in ['state']:
-                                                                for seed in [10]:
-                                                                    exp_name = f"{datetime.today().strftime('%Y%m%d')}_mcfac_{env_name}_obs_norm={obs_norm_type}_alpha={alpha}_distill={distill_type}_mixup={distill_mixup}_critic_loss={critic_loss_type}_critic_noise={critic_noise_type}_expectile={expectile}_q_agg={q_agg}_norm_q={normalize_q_loss}_reward_layer_norm={reward_layer_norm}_use_target_reward={use_target_reward}_reward={reward_type}"
-                                                                    log_dir = os.path.expanduser(
-                                                                        f"{log_root_dir}/exp_logs/ogbench_logs/mcfac/{exp_name}/{seed}")
+                    for batch_size in [256]:
+                        for alpha in [30, 10, 3]:
+                            for distill_type in ['fwd_sample']:
+                                for distill_mixup in [False]:
+                                    for critic_loss_type in ['expectile']:
+                                        for critic_noise_type in ['normal']:
+                                            for expectile in [0.9, 0.95, 0.99]:
+                                                for q_agg in ['min']:
+                                                    for normalize_q_loss in [True, False]:
+                                                        for reward_layer_norm in [True]:
+                                                            for use_target_reward in [True, False]:
+                                                                for reward_type in ['state']:
+                                                                    for seed in [10]:
+                                                                        exp_name = f"{datetime.today().strftime('%Y%m%d')}_mcfac_{env_name}_obs_norm={obs_norm_type}_discount={discount}_bs={batch_size}_alpha={alpha}_distill={distill_type}_mixup={distill_mixup}_critic_loss={critic_loss_type}_critic_noise={critic_noise_type}_expectile={expectile}_q_agg={q_agg}_norm_q={normalize_q_loss}_reward_layer_norm={reward_layer_norm}_use_target_reward={use_target_reward}_reward={reward_type}"
+                                                                        log_dir = os.path.expanduser(
+                                                                            f"{log_root_dir}/exp_logs/ogbench_logs/mcfac/{exp_name}/{seed}")
 
-                                                                    # change the log folder of slurm executor
-                                                                    submitit_log_dir = os.path.join(
-                                                                        os.path.dirname(log_dir),
-                                                                        'submitit')
-                                                                    executor._executor.folder = Path(
-                                                                        submitit_log_dir).expanduser().absolute()
+                                                                        # change the log folder of slurm executor
+                                                                        submitit_log_dir = os.path.join(
+                                                                            os.path.dirname(log_dir),
+                                                                            'submitit')
+                                                                        executor._executor.folder = Path(
+                                                                            submitit_log_dir).expanduser().absolute()
 
-                                                                    cmds = f"""
-                                                                        unset PYTHONPATH;
-                                                                        source $HOME/.zshrc;
-                                                                        conda activate ogbench;
-                                                                        which python;
-                                                                        echo $CONDA_PREFIX;
-        
-                                                                        echo job_id: $SLURM_ARRAY_JOB_ID;
-                                                                        echo task_id: $SLURM_ARRAY_TASK_ID;
-                                                                        squeue -j $SLURM_JOB_ID -o "%.18i %.9P %.8j %.8u %.2t %.6D %.5C %.11m %.11l %.12N";
-                                                                        echo seed: {seed};
-        
-                                                                        export PROJECT_DIR=$PWD;
-                                                                        export PYTHONPATH=$HOME/research/ogbench/impls;
-                                                                        export PATH="$PATH":"$CONDA_PREFIX"/bin;
-                                                                        export CUDA_VISIBLE_DEVICES=0;
-                                                                        export MUJOCO_GL=egl;
-                                                                        export PYOPENGL_PLATFORM=egl;
-                                                                        export EGL_DEVICE_ID=0;
-                                                                        export WANDB_API_KEY=bbb3bca410f71c2d7cfe6fe0bbe55a38d1015831;
-                                                                        export D4RL_SUPPRESS_IMPORT_ERROR=1;
-                                                                        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.mujoco/mujoco210/bin:/usr/lib/nvidia;
-                                                                        export XLA_FLAGS=--xla_gpu_triton_gemm_any=true;
-        
-                                                                        rm -rf {log_dir};
-                                                                        mkdir -p {log_dir};
-                                                                        python $PROJECT_DIR/impls/main_rl.py \
-                                                                            --enable_wandb=1 \
-                                                                            --env_name={env_name} \
-                                                                            --obs_norm_type={obs_norm_type} \
-                                                                            --eval_episodes=50 \
-                                                                            --dataset_class=GCDataset \
-                                                                            --agent=impls/agents/mcfac.py \
-                                                                            --agent.discount={discount} \
-                                                                            --agent.alpha={alpha} \
-                                                                            --agent.num_flow_steps=10 \
-                                                                            --agent.distill_type={distill_type} \
-                                                                            --agent.distill_mixup={distill_mixup} \
-                                                                            --agent.critic_loss_type={critic_loss_type} \
-                                                                            --agent.critic_noise_type={critic_noise_type} \
-                                                                            --agent.expectile={expectile} \
-                                                                            --agent.q_agg={q_agg} \
-                                                                            --agent.reward_layer_norm={reward_layer_norm} \
-                                                                            --agent.actor_layer_norm=False \
-                                                                            --agent.normalize_q_loss={normalize_q_loss} \
-                                                                            --agent.use_target_reward={use_target_reward} \
-                                                                            --agent.reward_type={reward_type} \
-                                                                            --seed={seed} \
-                                                                            --save_dir={log_dir} \
-                                                                        2>&1 | tee {log_dir}/stream.log;
-        
-                                                                        export SUBMITIT_RECORD_FILENAME={log_dir}/submitit_"$SLURM_ARRAY_JOB_ID"_"$SLURM_ARRAY_TASK_ID".txt;
-                                                                        echo "{submitit_log_dir}/"$SLURM_ARRAY_JOB_ID"_"$SLURM_ARRAY_TASK_ID"_submitted.pkl" >> "$SUBMITIT_RECORD_FILENAME";
-                                                                        echo "{submitit_log_dir}/"$SLURM_ARRAY_JOB_ID"_submission.sh" >> "$SUBMITIT_RECORD_FILENAME";
-                                                                        echo "{submitit_log_dir}/"$SLURM_ARRAY_JOB_ID"_"$SLURM_ARRAY_TASK_ID"_0_log.out" >> "$SUBMITIT_RECORD_FILENAME";
-                                                                        echo "{submitit_log_dir}/"$SLURM_ARRAY_JOB_ID"_"$SLURM_ARRAY_TASK_ID"_0_result.pkl" >> "$SUBMITIT_RECORD_FILENAME";
-                                                                    """
+                                                                        cmds = f"""
+                                                                            unset PYTHONPATH;
+                                                                            source $HOME/.zshrc;
+                                                                            conda activate ogbench;
+                                                                            which python;
+                                                                            echo $CONDA_PREFIX;
+            
+                                                                            echo job_id: $SLURM_ARRAY_JOB_ID;
+                                                                            echo task_id: $SLURM_ARRAY_TASK_ID;
+                                                                            squeue -j $SLURM_JOB_ID -o "%.18i %.9P %.8j %.8u %.2t %.6D %.5C %.11m %.11l %.12N";
+                                                                            echo seed: {seed};
+            
+                                                                            export PROJECT_DIR=$PWD;
+                                                                            export PYTHONPATH=$HOME/research/ogbench/impls;
+                                                                            export PATH="$PATH":"$CONDA_PREFIX"/bin;
+                                                                            export CUDA_VISIBLE_DEVICES=0;
+                                                                            export MUJOCO_GL=egl;
+                                                                            export PYOPENGL_PLATFORM=egl;
+                                                                            export EGL_DEVICE_ID=0;
+                                                                            export WANDB_API_KEY=bbb3bca410f71c2d7cfe6fe0bbe55a38d1015831;
+                                                                            export D4RL_SUPPRESS_IMPORT_ERROR=1;
+                                                                            export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.mujoco/mujoco210/bin:/usr/lib/nvidia;
+                                                                            export XLA_FLAGS=--xla_gpu_triton_gemm_any=true;
+            
+                                                                            rm -rf {log_dir};
+                                                                            mkdir -p {log_dir};
+                                                                            python $PROJECT_DIR/impls/main_rl.py \
+                                                                                --enable_wandb=1 \
+                                                                                --env_name={env_name} \
+                                                                                --obs_norm_type={obs_norm_type} \
+                                                                                --eval_episodes=50 \
+                                                                                --dataset_class=GCDataset \
+                                                                                --agent=impls/agents/mcfac.py \
+                                                                                --agent.discount={discount} \
+                                                                                --agent.batch_size={batch_size} \
+                                                                                --agent.alpha={alpha} \
+                                                                                --agent.num_flow_steps=10 \
+                                                                                --agent.distill_type={distill_type} \
+                                                                                --agent.distill_mixup={distill_mixup} \
+                                                                                --agent.critic_loss_type={critic_loss_type} \
+                                                                                --agent.critic_noise_type={critic_noise_type} \
+                                                                                --agent.expectile={expectile} \
+                                                                                --agent.q_agg={q_agg} \
+                                                                                --agent.reward_layer_norm={reward_layer_norm} \
+                                                                                --agent.actor_layer_norm=False \
+                                                                                --agent.normalize_q_loss={normalize_q_loss} \
+                                                                                --agent.use_target_reward={use_target_reward} \
+                                                                                --agent.reward_type={reward_type} \
+                                                                                --seed={seed} \
+                                                                                --save_dir={log_dir} \
+                                                                            2>&1 | tee {log_dir}/stream.log;
+            
+                                                                            export SUBMITIT_RECORD_FILENAME={log_dir}/submitit_"$SLURM_ARRAY_JOB_ID"_"$SLURM_ARRAY_TASK_ID".txt;
+                                                                            echo "{submitit_log_dir}/"$SLURM_ARRAY_JOB_ID"_"$SLURM_ARRAY_TASK_ID"_submitted.pkl" >> "$SUBMITIT_RECORD_FILENAME";
+                                                                            echo "{submitit_log_dir}/"$SLURM_ARRAY_JOB_ID"_submission.sh" >> "$SUBMITIT_RECORD_FILENAME";
+                                                                            echo "{submitit_log_dir}/"$SLURM_ARRAY_JOB_ID"_"$SLURM_ARRAY_TASK_ID"_0_log.out" >> "$SUBMITIT_RECORD_FILENAME";
+                                                                            echo "{submitit_log_dir}/"$SLURM_ARRAY_JOB_ID"_"$SLURM_ARRAY_TASK_ID"_0_result.pkl" >> "$SUBMITIT_RECORD_FILENAME";
+                                                                        """
 
-                                                                    cmd_func = submitit.helpers.CommandFunction([
-                                                                        "/bin/zsh", "-c",
-                                                                        cmds,
-                                                                    ], verbose=True)
+                                                                        cmd_func = submitit.helpers.CommandFunction([
+                                                                            "/bin/zsh", "-c",
+                                                                            cmds,
+                                                                        ], verbose=True)
 
-                                                                    executor.submit(cmd_func)
+                                                                        executor.submit(cmd_func)
 
 
 if __name__ == "__main__":
