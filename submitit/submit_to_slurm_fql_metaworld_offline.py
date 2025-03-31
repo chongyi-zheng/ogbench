de5a5809be1a8b4e5cea360d9f001d1512356f19
Chongyi Zheng <cz8792@rinse.cs.princeton.edu>
2025-03-31 13:51:06 -0400

    update submitit/submit_to_slurm_fql_metaworld_offline.py

--- submitit/submit_to_slurm_fql_metaworld_offline.py
+++ submitit/submit_to_slurm_fql_metaworld_offline.py
@@ -40,7 +40,7 @@ def main():
         slurm_mem="8G",
         slurm_gpus_per_node=1,
         slurm_stderr_to_stdout=True,
-        slurm_array_parallelism=40,
+        slurm_array_parallelism=20,
     )
 
     # ddpgbc hyperparameters: discount, alpha, num_flow_steps, normalize_q_loss
