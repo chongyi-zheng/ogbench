99f375171419e91d1152acf2f7bb6effccb51762
chongyi-zheng <chongyiz@andrew.cmu.edu>
2025-04-05 14:31:27 -0400

    update submitit/submit_to_slurm_sarsa_ifql_dmc_offline2offline.py

--- submitit/submit_to_slurm_sarsa_ifql_dmc_offline2offline.py
+++ submitit/submit_to_slurm_sarsa_ifql_dmc_offline2offline.py
@@ -69,10 +69,10 @@ def main():
             for obs_norm_type in ['normal']:
                 for lr in [3e-4]:
                     for tau in [0.005]:  # 1.0 doesn't work better than 0.005
-                        for alpha in [3.0, 0.3, 0.03]:
+                        for alpha in [3.0, 0.3]:
                             for num_flow_goals in [16]:
-                                for actor_freq in [1, 2, 4]:
-                                    for expectile in [0.65, 0.75, 0.8, 0.85]:
+                                for actor_freq in [1, 4]:
+                                    for expectile in [0.7, 0.75, 0.8, 0.85]:
                                         for q_agg in ['mean', 'min']:
                                             for normalize_q_loss in [False]:
                                                 for critic_fm_loss_type in ['sarsa_squared_stepwise']:
