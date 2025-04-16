864c110035c2f4cc332a224a8f94c7a5806c53fa
Chongyi Zheng <cz8792@soak.cs.princeton.edu>
2025-04-16 19:42:54 -0400

    update submitit/submit_to_slurm_crl_infonce_offline2offline.py

--- submitit/submit_to_slurm_crl_infonce_offline2offline.py
+++ submitit/submit_to_slurm_crl_infonce_offline2offline.py
@@ -53,17 +53,20 @@ def main():
             # "cube-double-play-singletask-task2-v0",
             # "scene-play-singletask-task2-v0",
             "cheetah_run",
-            "walker_walk",
             "cheetah_run_backward",
-            "walker_flip",
-            "quadruped_jump",
-            "jaco_reach_top_left",
+            "cheetah_walk",
+            "cheetah_walk_backward",
+            # "walker_walk",
+            # "cheetah_run_backward",
+            # "walker_flip",
+            # "quadruped_jump",
+            # "jaco_reach_top_left",
         ]:
             for obs_norm_type in ['normal']:
-                for alpha in [30.0, 3.0, 0.3, 0.03, 0.003, 0.0003]:
+                for alpha in [0.03, 0.003]:
                     for reward_type in ['state']:
                         for actor_freq in [4]:
-                            for seed in [10, 20]:
+                            for seed in [30, 40, 50, 60, 70]:
                                 exp_name = f"{datetime.today().strftime('%Y%m%d')}_crl_infonce_offline2offline_{env_name}_obs_norm_type={obs_norm_type}_alpha={alpha}_reward_type={reward_type}_actor_freq={actor_freq}"
                                 log_dir = os.path.expanduser(
                                     f"{log_root_dir}/exp_logs/ogbench_logs/crl_infonce_offline2offline/{exp_name}/{seed}")
