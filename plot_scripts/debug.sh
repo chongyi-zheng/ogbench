644e50097db0568f76c338ca37c5ed15025f27e3
Chongyi Zheng <cz8792@wash.cs.princeton.edu>
2025-06-05 01:18:33 -0400

    update results and submitis

--- plot_scripts/debug.sh
+++ plot_scripts/debug.sh
@@ -1,4 +1,4 @@
 rsync -av --progress --exclude='*.pkl' \
       -e ssh \
-      'cz8792@neuronic.cs.princeton.edu:/n/fs/prl-chongyiz/exp_logs/ogbench_logs/sarsa_ifql_vib_gpi_offline2offline/20250522_sarsa_ifql_vib_gpi_offline2offline*' \
+      'cz8792@neuronic.cs.princeton.edu:/n/fs/prl-chongyiz/exp_logs/ogbench_logs/sarsa_ifql_vib_gpi_offline2offline/20250530_sarsa_ifql_vib_gpi_offline2offline_cube-double-play*' \
       /n/fs/rl-chongyiz/exp_logs/ogbench_logs/sarsa_ifql_vib_gpi_offline2offline/
