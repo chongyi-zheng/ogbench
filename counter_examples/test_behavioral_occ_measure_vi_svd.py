acca2f079a106816dec7800f4af891891ff867c0
chongyi-zheng <chongyiz@andrew.cmu.edu>
2025-08-27 16:05:35 -0400

    compute avg_abs_err for adversarial rewards

--- counter_examples/test_behavioral_occ_measure_vi_svd.py
+++ counter_examples/test_behavioral_occ_measure_vi_svd.py
@@ -25,7 +25,7 @@ flags.DEFINE_integer('repr_dim', 16, 'Forward backward representation dimensions
 flags.DEFINE_integer('normalized_latent', 0, 'Whether to normalize the latents and backward representations.')
 flags.DEFINE_integer('num_eval_latents', 8, 'Number of evaluation latents.')
 flags.DEFINE_float('orthonorm_coef', 0.0, 'Orthonormalization regularization coefficient.')
-flags.DEFINE_float('null_reward_scale', 1.0, 'Scalar for the null reward function.')
+flags.DEFINE_float('reward_scale', 1.0, 'Scalar for the reward function.')
 
 
 def step(observation, action):
@@ -36,54 +36,36 @@ def step(observation, action):
     return next_observation
 
 
-def compute_gt_fb_reprs(behavioral_policy):
+def get_transition_probs(dtype):
     transition_probs_sa = np.zeros([FLAGS.num_observations, FLAGS.num_actions, FLAGS.num_observations])
     for obs in range(FLAGS.num_observations):
         for action in range(FLAGS.num_actions):
             next_obs = step(obs, action)
             transition_probs_sa[obs, action, next_obs] += 1
     transition_probs_sa = transition_probs_sa / np.sum(transition_probs_sa, axis=-1, keepdims=True)
-    transition_probs_sa = jnp.asarray(transition_probs_sa, dtype=behavioral_policy.dtype)
+    transition_probs_sa = jnp.asarray(transition_probs_sa, dtype=dtype)
+    return transition_probs_sa
+    
 
+def compute_fb_reprs(policy, transition_probs_sa):
     neg_marg_sa = jnp.ones(
         [FLAGS.num_observations, FLAGS.num_actions],
-        dtype=behavioral_policy.dtype
+        dtype=policy.dtype
     ) / (FLAGS.num_observations * FLAGS.num_actions)
 
     # value iteration
-    # Build T_pi: [S, A, S, A] with T_pi[s, a, s', a'] = P(s'|s,a) * pi[a'|s']
-    # T_pi = jnp.einsum('ijk,kl->ijkl', transition_probs_sa, policy)  # [S, A, S, A]
-    T_pi = transition_probs_sa[..., None] * behavioral_policy[None, None]  # [S, A, S, A]
-    T_flat = T_pi.reshape(FLAGS.num_observations * FLAGS.num_actions,
+    # Build P_pi: [S, A, S, A] with P_pi[s, a, s', a'] = P(s'|s,a) * pi[a'|s']
+    # P_pi = jnp.einsum('ijk,kl->ijkl', transition_probs_sa, policy)  # [S, A, S, A]
+    P_pi = transition_probs_sa[..., None] * policy[None, None]  # [S, A, S, A]
+    P_flat = P_pi.reshape(FLAGS.num_observations * FLAGS.num_actions,
                           FLAGS.num_observations * FLAGS.num_actions)
-    I = jnp.eye(FLAGS.num_observations * FLAGS.num_actions, dtype=behavioral_policy.dtype)
+    I = jnp.eye(FLAGS.num_observations * FLAGS.num_actions, dtype=policy.dtype)
     # solve (I - discount * T) X = (1-discount) * I for X
-    gt_sr_sa_sa = (1.0 - FLAGS.discount) * jnp.linalg.inv(I - FLAGS.discount * T_flat)  # [S * A, S * A]
+    gt_sr_sa_sa = (1.0 - FLAGS.discount) * jnp.linalg.inv(I - FLAGS.discount * P_flat)  # [S * A, S * A]
     # gt_sr_sa_sa = gt_sr_sa_sa_flat.reshape(FLAGS.num_observations, FLAGS.num_actions,
     #                                        FLAGS.num_observations, FLAGS.num_actions)
 
     gt_ratios = gt_sr_sa_sa / neg_marg_sa.reshape(-1)[None]  # [S * A, S * A]
-
-    def compute_gt_occ_measure(policy):
-        gt_sr_sa_sa = jnp.zeros([FLAGS.num_observations, FLAGS.num_actions, FLAGS.num_observations, FLAGS.num_actions])
-        for _ in range(1000):
-            gt_sr_sa_sa_new = (
-                (1 - FLAGS.discount) * jnp.eye(FLAGS.num_observations * FLAGS.num_actions).reshape(
-            FLAGS.num_observations, FLAGS.num_actions, FLAGS.num_observations, FLAGS.num_actions)
-                + FLAGS.discount * jnp.einsum('ijk,klm->ijlm', transition_probs_sa,
-                                              jnp.sum(policy[..., None, None] * gt_sr_sa_sa, axis=1))
-            )
-
-            if jnp.max(jnp.abs(gt_sr_sa_sa_new - gt_sr_sa_sa)) < 1e-10:
-                gt_sr_sa_sa = gt_sr_sa_sa_new
-                break
-            gt_sr_sa_sa = gt_sr_sa_sa_new
-
-        return gt_sr_sa_sa
-
-    # gt_sr_sa_sa_tmp = compute_gt_occ_measure(behavioral_policy)
-    # gt_sr_sa_sa_tmp = gt_sr_sa_sa_tmp.reshape(FLAGS.num_observations * FLAGS.num_actions,
-    #                                           FLAGS.num_observations * FLAGS.num_actions)
 
     u, s, vt = jnp.linalg.svd(gt_ratios, full_matrices=False)
     assert jnp.allclose(u @ jnp.diag(s) @ vt, gt_ratios)  # only works with float64
@@ -112,26 +94,62 @@ def compute_gt_fb_reprs(behavioral_policy):
     return info
 
 
+def compute_qs(policy, rewards, transition_probs_sa):
+    # value iteration
+    # Build P_pi: [S, A, S, A] with P_pi[s, a, s', a'] = P(s'|s,a) * pi[a'|s']
+    P_pi = transition_probs_sa[..., None] * policy[None, None]  # [S, A, S, A]
+    P_flat = P_pi.reshape(FLAGS.num_observations * FLAGS.num_actions,
+                          FLAGS.num_observations * FLAGS.num_actions)
+    R = rewards.astype(policy.dtype).reshape(FLAGS.num_observations * FLAGS.num_actions)  # [S * A]
+    I = jnp.eye(FLAGS.num_observations * FLAGS.num_actions, dtype=policy.dtype)  # [S * A, S * A]
+    # solve (I - discount * T) X = (1-discount) * R for X
+    q_flat = (1.0 - FLAGS.discount) * R @ jnp.linalg.inv(I - FLAGS.discount * P_flat)  # [S * A, S * A]
+    q = q_flat.reshape(FLAGS.num_observations, FLAGS.num_actions)
+
+    return q
+
+
 def main(_):
     # we use jnp.float64 to enable accurate SVD.
     jax.config.update('jax_enable_x64', True)
 
-    # rng = jax.random.key(np.random.randint(2 ** 32))
-    # rng, latent_rng = jax.random.split(rng, 2)
-    # latents = jax.random.normal(latent_rng, shape=(FLAGS.num_eval_latents, FLAGS.repr_dim))
-    # latents = latents / jnp.linalg.norm(latents, axis=-1, keepdims=True)
+    transition_probs_sa = get_transition_probs(jnp.float64)
     behavioral_policy = jnp.ones([FLAGS.num_observations, FLAGS.num_actions], dtype=jnp.float64) / FLAGS.num_actions
-    gt_fb_reprs = compute_gt_fb_reprs(behavioral_policy)
+    gt_fb_reprs = compute_fb_reprs(behavioral_policy, transition_probs_sa)
 
+    neg_marg_sa = gt_fb_reprs['neg_marg_sa']
+    forward_reprs = gt_fb_reprs['forward_reprs']
     backward_reprs = gt_fb_reprs['backward_reprs']
-    backward_reprs = backward_reprs.reshape(
+    backward_reprs_flat = backward_reprs.reshape(
         FLAGS.num_observations * FLAGS.num_actions, FLAGS.repr_dim)
 
-    backward_reprs = np.asarray(backward_reprs)
-    null_basis = scipy.linalg.null_space(backward_reprs.T).T
+    null_basis = scipy.linalg.null_space(np.asarray(backward_reprs_flat).T).T
     num_null_basis = null_basis.shape[0]
 
-    print(num_null_basis)
+    print("num_null_basis = {}".format(num_null_basis))
+    if num_null_basis > 0:
+        adversarial_rewards_flat = FLAGS.reward_scale * null_basis
+        adversarial_rewards = adversarial_rewards_flat.reshape(
+            num_null_basis, FLAGS.num_observations, FLAGS.num_actions)
+        reward_latents_flat = jnp.einsum(
+            'ij,ki->kj',
+            backward_reprs_flat * neg_marg_sa.reshape(-1)[..., None],
+            adversarial_rewards_flat,
+        )
+        reward_latents = reward_latents_flat.reshape(
+            num_null_basis, FLAGS.repr_dim
+        )
+        q_beta_ests = jnp.einsum('ijk,lk->lij', forward_reprs, reward_latents)
+        q_beta_gts = jax.vmap(compute_qs, in_axes=(None, 0, None))(
+            behavioral_policy, adversarial_rewards, transition_probs_sa)
+
+        avg_abs_err = jnp.mean(jnp.abs(q_beta_gts - q_beta_ests))
+
+        print("avg_abs_err = {}".format(avg_abs_err))
+    else:
+        pass
+
+    print("end")
 
 
 if __name__ == "__main__":
